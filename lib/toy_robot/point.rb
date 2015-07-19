1e363fb2cb8bf98f9cb9dd8d20858f6c6befdb7b
Christopher Atkins <christopherlionelatkins@gmail.com>
2015-07-19 15:58:12 +1000

    Added helper method to Point to calculate next step in a given direction

--- lib/toy_robot/point.rb
+++ lib/toy_robot/point.rb
@@ -8,5 +8,12 @@ module ToyRobot
       attribute :y, Integer
     end
 
+    def step_in_direction(direction)
+      new_x = x + direction.x_difference
+      new_y = y + direction.y_difference
+
+      Point.new x: new_x, y: new_y
+    end
+
   end
 end
--- spec/lib/toy_robot/point_spec.rb
+++ spec/lib/toy_robot/point_spec.rb
@@ -4,4 +4,18 @@ RSpec.describe ToyRobot::Point do
   it { is_expected.to respond_to :y }
   it { is_expected.to be_frozen }
 
+  describe '#step_in_direction' do
+    let(:starting_point) { described_class.new x: 1, y: 2 }
+    let(:direction) { ToyRobot::Direction.new name: 'onward', x_difference: 1, y_difference: -1 }
+    subject(:result) { starting_point.step_in_direction(direction) }
+
+    it "returns a new point" do
+      expect(result).not_to eq starting_point
+    end
+
+    it "adds the direction's X and Y differences to the new point" do
+      expect(result).to eq described_class.new(x: 2, y: 1)
+    end
+  end
+
 end
