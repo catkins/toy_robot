581ec850676d8136e5543caf02d8759738e7a8f7
Christopher Atkins <christopherlionelatkins@gmail.com>
2015-07-19 18:20:55 +1000

    Implemented Executor#call
    - Pardon the metaprogramming

--- lib/toy_robot/executor.rb
+++ lib/toy_robot/executor.rb
@@ -1,15 +1,36 @@
 module ToyRobot
   class Executor
 
+    AVAILABLE_COMMANDS = %i(left right move place report)
+
     attr_reader :robot
 
     def initialize(robot)
       @robot = robot
     end
 
-    def call(command, *args)
+    def call(command, args)
+      return unless AVAILABLE_COMMANDS.include? command
 
+      if args.any?
+        send command, *args
+      else
+        send command
+      end
     end
 
+    private
+
+    def place(x, y, direction)
+      position  = Point.new x: x, y: y
+      direction = Direction.by_name direction
+      robot.place position, direction
+    end
+
+    %i(left right move report).each do |command|
+      define_method(command) do
+        robot.public_send command
+      end
+    end
   end
 end
--- spec/lib/toy_robot/executor_spec.rb
+++ spec/lib/toy_robot/executor_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe ToyRobot::Executor do
 
-  let(:robot) { instance_double 'ToyRobot::Roboy' }
+  let(:robot) { instance_double 'ToyRobot::Robot' }
   subject(:executor) { described_class.new(robot) }
 
   it { is_expected.to respond_to :call }
@@ -13,11 +13,34 @@ RSpec.describe ToyRobot::Executor do
   end
 
   describe '#call' do
-    context 'left'
-    context 'right'
-    context 'move'
-    context 'place'
-    context 'report'
-  end
+    context 'unknown commands' do
+      it 'does nothing' do
+        expect { executor.call :garbage, [] }.not_to raise_error
+      end
+    end
 
+    context 'place' do
+      let(:expected_position) { ToyRobot::Point.new x: 2, y: 4 }
+      let(:expected_direction) { ToyRobot::Direction.by_name 'NORTH' }
+
+      it 'delegates to Robot#place' do
+        expect(robot).to receive(:place).with(expected_position, expected_direction)
+
+        executor.call :place, ["2", "4", "NORTH"]
+      end
+    end
+
+    %w(left right move report).each do |command|
+
+      context(command) do
+
+        it "delegates to Robot##{command}" do
+          expect(robot).to receive(command.to_sym)
+          executor.call command.to_sym, []
+        end
+
+      end
+    end
+
+  end
 end
