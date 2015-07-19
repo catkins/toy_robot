f1511af588992761ed6886fddd2486d02a4bbbb0
Christopher Atkins <christopherlionelatkins@gmail.com>
2015-07-19 16:37:32 +1000

    implemented place and move commands for robot

--- lib/toy_robot/robot.rb
+++ lib/toy_robot/robot.rb
@@ -1,10 +1,11 @@
 module ToyRobot
   class Robot
-    attr_reader :table, :compass
+    attr_reader :table, :compass, :position
 
     def initialize(table:, compass: Compass.new)
-      @table   = table
-      @compass = compass
+      @table    = table
+      @compass  = compass
+      @position = nil
     end
 
     def execute(command, arguments)
@@ -13,6 +14,35 @@ module ToyRobot
 
     def facing
       compass.bearing
+    end
+
+    def place(point, direction)
+      if table.inside_bounds?(point) && compass.has_direction?(direction)
+        @position        = point
+        @compass.bearing = direction
+      end
+
+      nil # command methods don't return anything
+    end
+
+    def move
+      return if not_on_table?
+
+      new_position = @position.step_in_direction(@compass.bearing)
+
+      if valid_position? new_position
+        @position = new_position
+      end
+    end
+
+    private
+
+    def valid_position?(point)
+      @table.inside_bounds?(point)
+    end
+
+    def not_on_table?
+      position.nil?
     end
 
   end
--- spec/lib/toy_robot/robot_spec.rb
+++ spec/lib/toy_robot/robot_spec.rb
@@ -26,6 +26,110 @@ RSpec.describe ToyRobot::Robot do
     end
   end
 
+  describe '#position' do
+    context 'robot has not yet been placed on table' do
+      its(:position) { is_expected.to be_nil }
+    end
+
+    context 'robot has been placed on a valid position on table' do
+      let(:valid_point) { ToyRobot::Point.new x: 3, y: 2 }
+      before { robot.place valid_point, ToyRobot::Direction.north }
+
+      its(:position) { is_expected.to eq valid_point }
+    end
+  end
+
+  describe '#place' do
+    let(:new_point)     { ToyRobot::Point.new x: 3, y: 2 }
+    let(:new_direction) { ToyRobot::Direction.east }
+
+    context 'with a valid point and direction' do
+
+      it "updates robot's position" do
+        expect {
+          robot.place(new_point, new_direction)
+        }.to change(robot, :position).from(nil).to(ToyRobot::Point.new x: 3, y: 2)
+      end
+
+      it "updates robot's facing" do
+        old_direction = ToyRobot::Direction.north
+        expect {
+          robot.place(new_point, new_direction)
+        }.to change(robot, :facing).from(old_direction).to(new_direction)
+      end
+    end
+
+    context 'with an invalid (off the table) point' do
+      let(:new_point)     { ToyRobot::Point.new x: 5, y: 6 }
+      let(:new_direction) { ToyRobot::Direction.east }
+
+      it "does not change robot's position" do
+        expect {
+          robot.place(new_point, new_direction)
+        }.not_to change(robot, :position)
+      end
+
+      it "does not change robot's facing" do
+        old_direction = ToyRobot::Direction.north
+        expect {
+          robot.place(new_point, new_direction)
+        }.not_to change(robot, :facing)
+      end
+    end
+
+    context 'with an invalid direction' do
+      let(:new_point)     { ToyRobot::Point.new x: 3, y: 2 }
+      let(:new_direction) { ToyRobot::Direction.new name: 'downside up' }
+
+      it "does not change robot's position" do
+        expect {
+          robot.place(new_point, new_direction)
+        }.not_to change(robot, :position)
+      end
+
+      it "does not change robot's facing" do
+        old_direction = ToyRobot::Direction.north
+        expect {
+          robot.place(new_point, new_direction)
+        }.not_to change(robot, :facing)
+      end
+    end
+  end
+
+  describe '#move' do
+    it { is_expected.to respond_to :move }
+
+    context 'before robot is placed on table' do
+      it "does not change robot's position" do
+        expect {
+          robot.move
+        }.not_to change(robot, :position)
+      end
+    end
+
+    context 'when robot is not on edge of table' do
+      let(:starting_point) { ToyRobot::Point.new x: 1, y: 1 }
+      before { robot.place starting_point, ToyRobot::Direction.north }
+
+      it 'updates position' do
+        expect { robot.move } .to change(robot, :position)
+          .from(starting_point).to(ToyRobot::Point.new x: 1, y: 2)
+      end
+    end
+
+    context 'when robot is on edge of table and facing the abyss' do
+      let(:starting_point) { ToyRobot::Point.new x: 0, y: 0 }
+      let(:direction) { ToyRobot::Direction.south }
+      before { robot.place starting_point, direction }
+
+      it "does not change robot's position" do
+        expect {
+          robot.place(starting_point, direction)
+        }.not_to change(robot, :position)
+      end
+    end
+  end
+
   describe '#execute' do
     it { is_expected.to respond_to :execute }
 
