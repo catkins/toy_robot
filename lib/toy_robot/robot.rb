85ae3ed181bfbb9081df69bd33bb092edf3fca16
Christopher Atkins <christopherlionelatkins@gmail.com>
2015-07-19 15:56:46 +1000

    Robot now has a built in compass

--- lib/toy_robot/robot.rb
+++ lib/toy_robot/robot.rb
@@ -1,14 +1,18 @@
 module ToyRobot
   class Robot
-    attr_reader :table, :facing
+    attr_reader :table, :compass
 
-    def initialize(table:)
-      @table  = table
-      @facing = :nowhere
+    def initialize(table:, compass: Compass.new)
+      @table   = table
+      @compass = compass
     end
 
     def execute(command, arguments)
       puts "#{command.inspect} => #{arguments}"
+    end
+
+    def facing
+      compass.bearing
     end
 
   end
--- spec/lib/toy_robot/robot_spec.rb
+++ spec/lib/toy_robot/robot_spec.rb
@@ -2,17 +2,27 @@ require 'spec_helper'
 
 RSpec.describe ToyRobot::Robot do
 
-  let(:table) { ToyRobot::Table.new width: 5, height: 5 }
+  let(:table)   { ToyRobot::Table.new width: 5, height: 5 }
+  let(:compass) { ToyRobot::Compass.new }
 
-  subject(:robot) { described_class.new(table: table) }
+  subject(:robot) { described_class.new(table: table, compass: compass) }
 
   it { is_expected.not_to be_nil }
+  it { is_expected.to respond_to :compass }
 
   describe '#facing' do
     it { is_expected.to respond_to :facing }
 
-    context 'before robot has been placed' do
-      its(:facing) { is_expected.to eq :nowhere }
+    it 'begins facing north' do
+      expect(robot.facing).to eq ToyRobot::Direction.north
+    end
+
+    it "delegates to the robot's compass" do
+      expected_direction = ToyRobot::Direction.new name: 'Onward and upward'
+      expect(compass).to receive(:bearing).and_return expected_direction
+
+      result = robot.facing
+      expect(result).to eq(expected_direction)
     end
   end
 
